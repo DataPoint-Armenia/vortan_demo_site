c1eb910e86daafc0df478baa1577b62808b429a4
sourencho <souren@papazian.info>
2021-06-24 23:49:51 -0700

    Attempt to access http from https

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,21 @@
 import './App.css';
+import {Helmet} from "react-helmet";
 
 import Header from "./components/Header";
 import Main from "./components/Main";
 
 function App() {
-  return (
-      <>
-          <Header />
-          <Main />
+    return (
+    <>
+        <Helmet>
+            <meta charSet="utf-8" />
+            <title>Vortan Demo</title>
+            <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+        </Helmet>
+        <Header />
+        <Main />
       </>
-  );
+    );
 }
 
 export default App;
