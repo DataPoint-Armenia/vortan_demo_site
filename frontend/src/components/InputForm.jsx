21d9e7cdae2e9f7293861ab5fdeb93b4d66d5816
sourencho <souren@papazian.info>
2021-06-26 00:19:56 -0700

    Add status

--- frontend/src/components/InputForm.jsx
+++ frontend/src/components/InputForm.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Table from 'react-bootstrap/Table';
+import Alert from 'react-bootstrap/Alert';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
 import Results from "./Results"
 
@@ -10,6 +14,8 @@ export const ResultsContext = React.createContext({ results: []})
 const InputForm = () => {
     const [input, getResults] = React.useState("")
     const [results, setResults] = React.useState([])
+    const [alertVariant, setAlertVariant] = React.useState("secondary");
+    const [alertText, setAlertText] = React.useState("Status");
 
     const handleInput = event  => {
         getResults(event.target.value)
@@ -21,6 +27,8 @@ const InputForm = () => {
           "word": input
         }
 
+        setAlertVariant("primary")
+        setAlertText("Loading")
         fetch("https://vortan-api.herokuapp.com/suggest", {
             method: "POST",
             headers: { "Content-Type": "application/json; charset=UTF-8" },
@@ -29,15 +37,21 @@ const InputForm = () => {
             response.json().then((x) => {
                 setResults(x.suggest)
             });
-        })
+            setAlertVariant("success")
+            setAlertText("Success")
+        }).catch((error) => {
+            console.error('Error:', error);
+            setAlertVariant("danger")
+            setAlertText("Error")
+        });
     }
 
     const style = {
         padding: "1em",
     };
     return (
-        <>
-            <Form style={style} onSubmit={handleSubmit}>
+        <div style={style}>
+            <Form onSubmit={handleSubmit}>
                 <Form.Group>
                     <Form.Label>Input Term</Form.Label>
                     <Form.Control type="text" placeholder="տպրոց" onChange={handleInput.bind(this)} />
@@ -45,14 +59,25 @@ const InputForm = () => {
                         Only supports single words for now
                     </Form.Text>
                 </Form.Group>
-                <Button variant="primary" type="submit">
-                    Submit
-                </Button>
+                <Container>
+                    <Row>
+                        <Col>
+                            <Button variant="primary" type="submit">
+                                Submit
+                            </Button>
+                        </Col>
+                        <Col>
+                            <Alert variant={alertVariant}>
+                                {alertText}
+                            </Alert>
+                        </Col>
+                    </Row>
+                </Container>
             </Form>
             <ResultsContext.Provider value={{results}}>
                 <Results />
             </ ResultsContext.Provider>
-        </>
+        </div>
     );
 };
 
--- frontend/src/components/Results.jsx
+++ frontend/src/components/Results.jsx
@@ -5,30 +5,24 @@ import { ResultsContext } from "./InputForm";
 export default function Results() {
     const { results } = React.useContext(ResultsContext)
 
-    const style = {
-        padding: "1em",
-    };
-
     return (
-        <div style={style}>
-            <Table striped bordered hover>
-                <thead>
-                    <tr>
-                        <th>Suggest</th>
-                        <th>Distance</th>
-                        <th>Score</th>
-                    </tr>
-                </thead>
-                <tbody>
-                {results.map((result, index) => (
-                    <tr key={index}>
-                        <td>{result.term}</td>
-                        <td>{result.distance}</td>
-                        <td>{result.score}</td>
-                    </tr>
-                ))}
-                </tbody>
-            </Table>
-        </div>
+        <Table striped bordered hover>
+            <thead>
+                <tr>
+                    <th>Suggest</th>
+                    <th>Distance</th>
+                    <th>Score</th>
+                </tr>
+            </thead>
+            <tbody>
+            {results.map((result, index) => (
+                <tr key={index}>
+                    <td>{result.term}</td>
+                    <td>{result.distance}</td>
+                    <td>{result.score}</td>
+                </tr>
+            ))}
+            </tbody>
+        </Table>
     )
 }
